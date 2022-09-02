1b4ca40aa31d7e5da703d2206dab59e93392a8f4
camelinx <camelinx@gmail.com>
2022-09-01 20:50:05 -0700

    Reorganized v4 test cases

--- v4_test.go
+++ v4_test.go
@@ -25,15 +25,15 @@ func validatev4Addr( t *testing.T, saddr string, exip string, exmask string, neg
     }
 }
 
-func TestV4( t *testing.T )( ) {
-    rand.Seed( time.Now( ).UnixNano( ) )
-
+func basicV4Tests( t *testing.T )( ) {
     validatev4Addr( t, "", "", "", true )
     validatev4Addr( t, "192.168.128.40/33", "", "", true )
     validatev4Addr( t, "256.168.128.40/32", "", "", true )
     validatev4Addr( t, "2001:db8:a0b:12f0::1", "", "", true )
     validatev4Addr( t, "2001:db8:a0b:12f0::1/64", "", "", true )
+}
 
+func randomV4Tests( t *testing.T )( ) {
     tree := Init( )
 
     for class := ipv4AddrClassMin + 1; class < ipv4AddrClassMax; class++ {
@@ -108,4 +108,83 @@ func TestV4( t *testing.T )( ) {
             }
         }
     }
+}
+
+func extendedV4Tests( t *testing.T )( ) {
+    tree := Init( )
+
+    elemsMap := make( map[ string ]bool )
+
+    prefix := "192.168"
+
+    for i := 0; i < 255; i++ {
+        for j := 0; j < 255; j++ {
+            for m := 32; m > 24; m-- {
+                cidr := fmt.Sprintf( "%s.%d.%d/%d", prefix, i, j, m ) 
+
+                ip, mask, _ := getv4Addr( cidr )
+
+                value := fmt.Sprintf( "%s/%s", ip.String( ), mask.String( ) )
+
+                if _, exists := elemsMap[ value ]; !exists {
+                    res, err := tree.Insertv4( cidr, value )
+                    if err != nil || res != Ok {
+                        t.Fatalf( "Failed to insert %s, result = %v, %v", cidr, res, err )
+                    }
+
+                    elemsMap[ value ] = true
+                } else {
+                    res, err := tree.Insertv4( cidr, value )
+                    if err != nil || res != Dup {
+                        t.Fatalf( "Failed to identify duplicate %s, result = %v, %v", cidr, res, err )
+                    }
+                }
+            }
+        }
+    }
+
+    for i := 0; i < 255; i++ {
+        for j := 0; j < 255; j++ {
+                //addr := fmt.Sprintf( "%s.%d.%d/32", prefix, i, j )
+            for m := 32; m > 24; m-- {
+                cidr := fmt.Sprintf( "%s.%d.%d/%d", prefix, i, j, m )
+
+                ip, mask, _ := getv4Addr( cidr )
+
+                value := fmt.Sprintf( "%s/%s", ip.String( ), mask.String( ) )
+
+                if _, exists := elemsMap[ value ]; !exists {
+                    continue
+                }
+
+                res, saved, err := tree.Searchv4Exact( cidr )
+                if nil == err && Match == res {
+                    if saved.( string ) != value {
+                        t.Fatalf( "Search failed for %s, returned %s, expected %s", cidr, saved.( string ), value )
+                    }
+                } else {
+                    t.Fatalf( "Search failed for %s, %v/%v", cidr, res, err )
+                }
+
+                res, saved, err = tree.Deletev4( cidr )
+                if nil == err && Match == res {
+                    if saved.( string ) != value {
+                        t.Fatalf( "Delete failed for %s, returned %s, expected %s", cidr, saved.( string ), value )
+                    }
+                } else {
+                    t.Fatalf( "Delete failed for %s, %v/%v", cidr, res, err )
+                }
+
+                delete( elemsMap, value )
+            }
+        }
+    }
+}
+
+func TestV4( t *testing.T )( ) {
+    rand.Seed( time.Now( ).UnixNano( ) )
+
+    basicV4Tests( t )
+    randomV4Tests( t )
+    extendedV4Tests( t )
 }
